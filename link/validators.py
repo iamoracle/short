7eab2e90564f970d5561e4596b3351c6046c1ca9
smavity <smavityllc@gmail.com>
2021-01-10 18:09:20 +0100

    corrected all bugs

--- link/validators.py
+++ link/validators.py
@@ -2,16 +2,22 @@ from requests import get
 
 from requests.exceptions import RequestException
 
+from django.core.exceptions import ValidationError
+
+from django.utils.translation import gettext_lazy as _
+
 def url_exists(url):
 
     try:
 
-        response = requests.get(url)
+        response = get(url)
 
     except RequestException:
 
-        return False
+        raise ValidationError(_('invalid or inactive url'))
 
     status = response.status_code
 
-    return status >= 200 and status < 300
+    if status >= 200 and status < 300: return True
+    
+    else: raise ValidationError(_('url does not exists'))
